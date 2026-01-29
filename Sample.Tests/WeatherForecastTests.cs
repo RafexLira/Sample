5f3e11925f20e7fc38ab207a986e886e2cdc3c91
Rafael Lira <rafael.alexandrelira@hotmail.com>
2026-01-29 14:23:47 -0300

    primeiro-commit

---
+++ Sample.Tests/WeatherForecastTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Logging;
+using Sample.Api.Controllers;
+
+namespace SampleApi.Tests
+{
+    public class WeatherForecastTests
+    {
+        [Fact]
+        public void Get_Returns_Five_Items()
+        {
+            // Arrange
+            var logger = LoggerFactory.Create(builder => { })
+                                      .CreateLogger<WeatherForecastController>();
+
+            var controller = new WeatherForecastController(logger);
+
+            // Act
+            var result = controller.Get();
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(5, result.Count());
+        }
+    }
+}
